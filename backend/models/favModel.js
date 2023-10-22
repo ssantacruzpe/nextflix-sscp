a0fc1e0168cffcc4fbdddfac00a3f2469ea55148
ssantacruzpe <stephanysantacruz@protonmail.com>
2023-10-22 11:28:09 +0200

    fav Button basic structure

--- backend/models/favModel.js
+++ backend/models/favModel.js
@@ -1,9 +1,10 @@
-/*const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+
 
 const imageSchema = new mongoose.Schema({
-  imageUrl: String,
-  isFavorite: Boolean
+imageUrl: String,
+isFavorite: Boolean
 });
 
+
 module.exports = mongoose.model('Image', imageSchema);
-*/
---
+++ client/src/components/favButton.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import "./favButton.css";
+
+const FavoriteButton = ({ imageID }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    const fetchFavStatus = async () => {
+      try {
+        const response = await fetch(`/api/images/${imageID}/favorite`);
+        const data = await response.json();
+        setIsFavorite(data.isFavorite);
+      } catch (error) {
+        console.error("Error fetching fav status:", error);
+      }
+    };
+
+    fetchFavStatus();
+  }, [imageID]);
+
+  const toggleFav = async () => {
+    try {
+      if (isFavorite) {
+        await fetch(`/api/images/${imageID}/favorite`, {
+          method: "DELETE",
+        });
+      } else {
+        await fetch(`/api/images/${imageID}/favorite`, {
+          method: "PUT",
+        });
+      }
+      setIsFavorite(!isFavorite);
+    } catch (error) {
+      console.error("Error toggling fav status:", error);
+    }
+  };
+
+  return (
+    <button
+      className={`favorite-button ${isFavorite ? "favorite" : ""}`}
+      onClick={toggleFav}
+    >
+      <span role="img" aria-label="Favorite">★</span>
+    </button>
+  );
+};
+
+export default FavoriteButton;
